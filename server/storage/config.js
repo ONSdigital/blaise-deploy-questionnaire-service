1364428e023864f6c978c996dae35ff459178820
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-01-11 17:31:50 +0000

    Use local GCP Access token for GCP access when in dev env.

--- server/storage/config.js
+++ server/storage/config.js
@@ -1,17 +1,23 @@
 import {getEnvironmentVariables} from "../Config";
 import path from "path";
 
-const serviceKey = path.join(__dirname, "./keys.json");
-
 const Cloud = require("@google-cloud/storage");
-
-const { Storage } = Cloud;
-
+const {Storage} = Cloud;
 const {PROJECT_ID} = getEnvironmentVariables();
 
-const storage = new Storage({
-  projectId: PROJECT_ID,
-  keyFilename: serviceKey,
-});
+let storageConfig = {
+    projectId: PROJECT_ID,
+};
+
+if (process.env.NODE_ENV !== "production") {
+    console.log("Not Prod: Attempting to use local keys.json file");
+    const serviceKey = path.join(__dirname, "../../../keys.json");
+    storageConfig = {
+        projectId: PROJECT_ID,
+        keyFilename: serviceKey,
+    };
+}
+
+const storage = new Storage(storageConfig);
 
 module.exports = storage;
