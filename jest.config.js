c5d4849ee70abc76f6a3750b1bbc30a95bf818dc
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-01-12 12:15:31 +0000

    ENV Var config tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 process.env = Object.assign(process.env, {
-    VM_EXTERNAL_CLIENT_URL: "external-client-url",
-    VM_EXTERNAL_WEB_URL: "external-web-url",
-    BLAISE_API_URL: "mock",
-    VM_INTERNAL_URL: "internal-url",
+    BLAISE_API_URL: "mock-api",
+    PROJECT_ID: "a-project-name",
+    BUCKET_NAME: "unique-bucket"
 });
--- server/tests/config.test.js
+++ server/tests/config.test.js
@@ -10,29 +10,26 @@ describe("Config setup", () => {
 
 
     it("should return the correct environment variables", () => {
-        const {VM_EXTERNAL_CLIENT_URL, VM_EXTERNAL_WEB_URL, BLAISE_API_URL, CATI_DASHBOARD_URL} = getEnvironmentVariables();
+        const {BLAISE_API_URL, PROJECT_ID, BUCKET_NAME} = getEnvironmentVariables();
 
 
-        expect(VM_EXTERNAL_CLIENT_URL).toBe("external-client-url");
-        expect(VM_EXTERNAL_WEB_URL).toBe("external-web-url");
-        expect(BLAISE_API_URL).toBe("mock");
-        expect(CATI_DASHBOARD_URL).toBe("https://external-web-url/Blaise");
+        expect(BLAISE_API_URL).toBe("mock-api");
+        expect(PROJECT_ID).toBe("a-project-name");
+        expect(BUCKET_NAME).toBe("unique-bucket");
     });
 
     it("should return variables with default string if variables are not defined", () => {
         process.env = Object.assign({
-            VM_EXTERNAL_CLIENT_URL: undefined,
-            VM_EXTERNAL_WEB_URL: undefined,
             BLAISE_API_URL: undefined,
-            VM_INTERNAL_URL: undefined,
+            PROJECT_ID: undefined,
+            BUCKET_NAME: undefined
         });
 
-        const {VM_EXTERNAL_CLIENT_URL, VM_EXTERNAL_WEB_URL, BLAISE_API_URL, CATI_DASHBOARD_URL} = getEnvironmentVariables();
+        const {BLAISE_API_URL, PROJECT_ID, BUCKET_NAME} = getEnvironmentVariables();
 
 
-        expect(VM_EXTERNAL_CLIENT_URL).toBe("ENV_VAR_NOT_SET");
-        expect(VM_EXTERNAL_WEB_URL).toBe("ENV_VAR_NOT_SET");
         expect(BLAISE_API_URL).toBe("ENV_VAR_NOT_SET");
-        expect(CATI_DASHBOARD_URL).toBe("https://undefined/Blaise");
+        expect(PROJECT_ID).toBe("ENV_VAR_NOT_SET");
+        expect(BUCKET_NAME).toBe("ENV_VAR_NOT_SET");
     });
 });
