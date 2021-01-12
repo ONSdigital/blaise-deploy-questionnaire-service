728469697423ba2a86079ff95cbd9efc17173b7d
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-01-12 12:23:47 +0000

    Formatting in test file

--- src/Components/ONSDesignSystem/ONSUpload.test.tsx
+++ src/Components/ONSDesignSystem/ONSUpload.test.tsx
@@ -4,29 +4,27 @@ import {ONSUpload} from "./ONSUpload";
 import Adapter from "enzyme-adapter-react-16";
 
 describe("ONS Upload Test", () => {
-    Enzyme.configure({ adapter: new Adapter() });
+    Enzyme.configure({adapter: new Adapter()});
 
-    const Props = {
-
-    }
+    const Props = {};
 
     const changeProps = {
         label: "Upload",
         description: "This is the upload",
         fileName: "file.csv",
-        fileID: 'file',
-        accept: 'yes?',
+        fileID: "file",
+        accept: "yes?",
         onChange: jest.fn()
-    }
+    };
 
     const undefinedChangeProps = {
         label: "Upload",
         description: "This is the upload",
         fileName: "file.csv",
-        fileID: 'file',
-        accept: 'yes?',
+        fileID: "file",
+        accept: "yes?",
         onChange: undefined
-    }
+    };
 
     function wrapper(render: any, props: any) {
         return render(
@@ -38,25 +36,24 @@ describe("ONS Upload Test", () => {
                 accept={props.accept}
                 onChange={props.onChange}>
             </ONSUpload>
-            
-        )
+        );
     }
 
     it("matches Snapshot", () => {
-        expect(wrapper(shallow, Props)).toMatchSnapshot()
+        expect(wrapper(shallow, Props)).toMatchSnapshot();
     });
 
     it("should render correctly", () => expect(wrapper(shallow, Props).exists()).toEqual(true));
 
     it("should handle a change", () => {
         //defined onchange
-        wrapper(mount, changeProps).find('input').simulate('change')
-        expect(changeProps.onChange).toHaveBeenCalled()        
-        
+        wrapper(mount, changeProps).find("input").simulate("change");
+        expect(changeProps.onChange).toHaveBeenCalled();
+
         //undefined onchange
-        wrapper(mount, undefinedChangeProps).find('input').simulate('change')
-        expect(undefinedChangeProps.onChange).toBeUndefined()        
+        wrapper(mount, undefinedChangeProps).find("input").simulate("change");
+        expect(undefinedChangeProps.onChange).toBeUndefined();
 
-    })
+    });
 
-})
+});
