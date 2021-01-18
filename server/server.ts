07a7c3b9f1be830d66f9d43d1cc3eb1421e24a57
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-01-18 17:38:15 +0000

    Call API to delete questionnaire and cleanup

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import express, {NextFunction, Request, Response} from "express";
-import axios from "axios";
+import axios, {AxiosRequestConfig} from "axios";
 import path from "path";
 import ejs from "ejs";
 import dotenv from "dotenv";
@@ -65,56 +65,33 @@ interface ResponseQuery extends Request {
 }
 
 server.get("/api/install", function (req: ResponseQuery, res: Response) {
-    logger(req, res);
-    req.log.info("/api/install endpoint called");
     const {filename} = req.query;
-    axios({
-        url: `http://${BLAISE_API_URL}/api/v1/serverparks/${SERVER_PARK}/instruments`,
-        method: "POST",
-        data: {
-            "instrumentName": filename.replace(/\.[a-zA-Z]*$/, ""),
-            "instrumentFile": filename,
-            "bucketPath": BUCKET_NAME
-        }
-    }).then((response) => {
-        req.log.info(`Call to /api/v1/serverparks/${SERVER_PARK}/instruments successful`);
-        res.status(response.status).json(response.data);
-    }).catch((error) => {
-        req.log.error(error, `Call to /api/v1/serverparks/${SERVER_PARK}/instruments failed`);
-        res.status(500).json(error);
-    });
+    const data = {
+        "instrumentName": filename.replace(/\.[a-zA-Z]*$/, ""),
+        "instrumentFile": filename,
+        "bucketPath": BUCKET_NAME
+    };
+    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments`;
+    SendBlaiseAPIRequest(req, res, url, "POST", data);
+
 });
 
 server.get("/api/instruments/:instrumentName/exists", function (req: ResponseQuery, res: Response) {
-    logger(req, res);
-    req.log.info("/api/instruments/:instrumentName/exists endpoint called");
     const {instrumentName} = req.params;
-    axios({
-        url: `http://${BLAISE_API_URL}/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}/exists`,
-        method: "GET"
-    }).then((response) => {
-        req.log.info(`Call to /api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}/exists`);
-        res.status(response.status).json(response.data);
-    }).catch((error) => {
-        req.log.error(error, `Call to /api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}/exists`);
-        res.status(500).json(error);
-    });
+    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}/exists`;
+    SendBlaiseAPIRequest(req, res, url, "GET");
 });
 
 server.get("/api/instruments/:instrumentName", function (req: ResponseQuery, res: Response) {
-    logger(req, res);
-    req.log.info("/api/instruments/:instrumentName endpoint called");
     const {instrumentName} = req.params;
-    axios({
-        url: `http://${BLAISE_API_URL}/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}`,
-        method: "GET"
-    }).then((response) => {
-        req.log.info(`Call to /api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}`);
-        res.status(response.status).json(response.data);
-    }).catch((error) => {
-        req.log.error(error, `Call to /api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}`);
-        res.status(500).json(error);
-    });
+    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}`;
+    SendBlaiseAPIRequest(req, res, url, "GET");
+});
+
+server.delete("/api/instruments/:instrumentName", function (req: ResponseQuery, res: Response) {
+    const {instrumentName} = req.params;
+    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}?name=${instrumentName}`;
+    SendBlaiseAPIRequest(req, res, url, "DELETE");
 });
 
 server.get("/api/instruments", function (req: ResponseQuery, res: Response) {
@@ -137,7 +114,6 @@ server.get("/api/instruments", function (req: ResponseQuery, res: Response) {
     });
 });
 
-//api/v1/serverparks/gusty/instruments
 // Health Check endpoint
 server.get("/health_check", async function (req: Request, res: Response) {
     console.log("Heath Check endpoint called");
@@ -153,6 +129,24 @@ server.use(function (err: Error, req: Request, res: Response, next: NextFunction
     req.log.error(err, err.message);
     res.render("../views/500.html", {});
 });
+
+function SendBlaiseAPIRequest(req: Request, res: Response, url: string, method: AxiosRequestConfig["method"], data:any = null) {
+    logger(req, res);
+    req.log.info(`${method} ${url} endpoint called`);
+    url = `http://${BLAISE_API_URL}${url}`;
+
+    axios({
+        url: url,
+        method: method,
+        data: data
+    }).then((response) => {
+        req.log.info(`Call to ${method} ${url}`);
+        res.status(response.status).json(response.data);
+    }).catch((error) => {
+        req.log.error(error, `Call to ${method} ${url}`);
+        res.status(500).json(error);
+    });
+}
 
 
 export default server;
