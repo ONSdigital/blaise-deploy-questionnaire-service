96230487cc7c97468f788945d5afed325c403c27
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-01-19 14:05:30 +0000

    Move Blaise API calls to separate router in server

---
+++ server/BlaiseAPI/index.ts
@@ -0,0 +1,90 @@
+import express, {Request, Response, Router} from "express";
+import {Instrument} from "../../Interfaces";
+import axios, {AxiosRequestConfig} from "axios";
+import Functions from "../Functions";
+import {EnvironmentVariables} from "../Config";
+
+export default function BlaiseAPIRouter(environmentVariables: EnvironmentVariables, logger: any): Router {
+    const {BLAISE_API_URL, BUCKET_NAME, SERVER_PARK}: EnvironmentVariables = environmentVariables;
+    const router = express.Router();
+
+    // Generic function to make requests to the API
+    function SendBlaiseAPIRequest(req: Request, res: Response, url: string, method: AxiosRequestConfig["method"], data: any = null) {
+        logger(req, res);
+        req.log.info(`${method} ${url} endpoint called`);
+        const fullUrl = `http://${BLAISE_API_URL}${url}`;
+
+        axios({
+            url: fullUrl,
+            method: method,
+            data: data
+        }).then((response) => {
+            req.log.info(`Call to ${method} ${url}`);
+            res.status(response.status).json(response.data);
+        }).catch((error) => {
+            req.log.error(error, `Call to ${method} ${url}`);
+            res.status(500).json(error);
+        });
+    }
+
+    interface ResponseQuery extends Request {
+        query: { filename: string }
+    }
+
+    // Call to install a specific instrument from a specified GCP bucket and file
+    router.get("/api/install", function (req: ResponseQuery, res: Response) {
+        const {filename} = req.query;
+        const data = {
+            "instrumentName": filename.replace(/\.[a-zA-Z]*$/, ""),
+            "instrumentFile": filename,
+            "bucketPath": BUCKET_NAME
+        };
+        const url = `/api/v1/serverparks/${SERVER_PARK}/instruments`;
+        SendBlaiseAPIRequest(req, res, url, "POST", data);
+
+    });
+
+    // Check if a specific instruments exists
+    router.get("/api/instruments/:instrumentName/exists", function (req: ResponseQuery, res: Response) {
+        const {instrumentName} = req.params;
+        const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}/exists`;
+        SendBlaiseAPIRequest(req, res, url, "GET");
+    });
+
+    // Get a specific instrument information
+    router.get("/api/instruments/:instrumentName", function (req: ResponseQuery, res: Response) {
+        const {instrumentName} = req.params;
+        const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}`;
+        SendBlaiseAPIRequest(req, res, url, "GET");
+    });
+
+    // Delete an instrument
+    router.delete("/api/instruments/:instrumentName", function (req: ResponseQuery, res: Response) {
+        const {instrumentName} = req.params;
+        const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}?name=${instrumentName}`;
+        SendBlaiseAPIRequest(req, res, url, "DELETE");
+    });
+
+    // Get list of all instruments installed in a specified server park
+    router.get("/api/instruments", function (req: ResponseQuery, res: Response) {
+        logger(req, res);
+        req.log.info("/api/instrument endpoint called");
+        axios({
+            url: `http://${BLAISE_API_URL}/api/v1/serverparks/${SERVER_PARK}/instruments`,
+            method: "GET"
+        }).then((response) => {
+            req.log.info({responseData: response.data}, `Call to /api/v1/serverparks/${SERVER_PARK}/instruments`);
+            const instruments: Instrument[] = response.data;
+            instruments.forEach(function (element: Instrument) {
+                element.fieldPeriod = Functions.field_period_to_text(element.name);
+            });
+            res.status(response.status).json(response.data);
+        }).catch((error) => {
+            req.log.error(error, `Call to /api/v1/serverparks/${SERVER_PARK}/instruments`);
+            res.status(500).json(error);
+        });
+    });
+
+    return router;
+}
+
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import express, {NextFunction, Request, Response} from "express";
-import axios, {AxiosRequestConfig} from "axios";
 import path from "path";
 import ejs from "ejs";
 import dotenv from "dotenv";
@@ -17,8 +16,7 @@ const logger = createLogger();
 server.use(logger);
 
 import {checkFile} from "./storage/helpers";
-import {Instrument} from "../Interfaces";
-import Functions from "./Functions";
+import BlaiseAPIRouter from "./BlaiseAPI";
 
 //axios.defaults.timeout = 10000;
 
@@ -26,15 +24,13 @@ import Functions from "./Functions";
 const buildFolder = "../../build";
 
 // load the .env variables in the server
-const {BLAISE_API_URL, BUCKET_NAME, SERVER_PARK} = getEnvironmentVariables();
+const environmentVariables = getEnvironmentVariables();
+const {BUCKET_NAME} = environmentVariables;
 
 // treat the index.html as a template and substitute the values at runtime
 server.set("views", path.join(__dirname, buildFolder));
 server.engine("html", ejs.renderFile);
-server.use(
-    "/static",
-    express.static(path.join(__dirname, `${buildFolder}/static`)),
-);
+server.use("/static", express.static(path.join(__dirname, `${buildFolder}/static`)));
 
 server.post("/upload", loadingByChunks);
 
@@ -60,59 +56,8 @@ server.get("/bucket", function (req: Request, res: Response) {
         });
 });
 
-interface ResponseQuery extends Request {
-    query: { filename: string }
-}
-
-server.get("/api/install", function (req: ResponseQuery, res: Response) {
-    const {filename} = req.query;
-    const data = {
-        "instrumentName": filename.replace(/\.[a-zA-Z]*$/, ""),
-        "instrumentFile": filename,
-        "bucketPath": BUCKET_NAME
-    };
-    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments`;
-    SendBlaiseAPIRequest(req, res, url, "POST", data);
-
-});
-
-server.get("/api/instruments/:instrumentName/exists", function (req: ResponseQuery, res: Response) {
-    const {instrumentName} = req.params;
-    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}/exists`;
-    SendBlaiseAPIRequest(req, res, url, "GET");
-});
-
-server.get("/api/instruments/:instrumentName", function (req: ResponseQuery, res: Response) {
-    const {instrumentName} = req.params;
-    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}`;
-    SendBlaiseAPIRequest(req, res, url, "GET");
-});
-
-server.delete("/api/instruments/:instrumentName", function (req: ResponseQuery, res: Response) {
-    const {instrumentName} = req.params;
-    const url = `/api/v1/serverparks/${SERVER_PARK}/instruments/${instrumentName}?name=${instrumentName}`;
-    SendBlaiseAPIRequest(req, res, url, "DELETE");
-});
-
-server.get("/api/instruments", function (req: ResponseQuery, res: Response) {
-    logger(req, res);
-    req.log.info("/api/instrument endpoint called");
-    axios({
-        url: `http://${BLAISE_API_URL}/api/v1/serverparks/${SERVER_PARK}/instruments`,
-        method: "GET"
-    }).then((response) => {
-        console.log(response);
-        req.log.info(`Call to /api/v1/serverparks/${SERVER_PARK}/instruments`);
-        const instruments: Instrument[] = response.data;
-        instruments.forEach(function (element: Instrument) {
-            element.fieldPeriod = Functions.field_period_to_text(element.name);
-        });
-        res.status(response.status).json(response.data);
-    }).catch((error) => {
-        req.log.error(error, `Call to /api/v1/serverparks/${SERVER_PARK}/instruments`);
-        res.status(500).json(error);
-    });
-});
+// All Endpoints calling the Blaise API
+server.use("/", BlaiseAPIRouter(environmentVariables, logger));
 
 // Health Check endpoint
 server.get("/health_check", async function (req: Request, res: Response) {
@@ -129,24 +74,5 @@ server.use(function (err: Error, req: Request, res: Response, next: NextFunction
     req.log.error(err, err.message);
     res.render("../views/500.html", {});
 });
-
-function SendBlaiseAPIRequest(req: Request, res: Response, url: string, method: AxiosRequestConfig["method"], data:any = null) {
-    logger(req, res);
-    req.log.info(`${method} ${url} endpoint called`);
-    url = `http://${BLAISE_API_URL}${url}`;
-
-    axios({
-        url: url,
-        method: method,
-        data: data
-    }).then((response) => {
-        req.log.info(`Call to ${method} ${url}`);
-        res.status(response.status).json(response.data);
-    }).catch((error) => {
-        req.log.error(error, `Call to ${method} ${url}`);
-        res.status(500).json(error);
-    });
-}
-
 
 export default server;
