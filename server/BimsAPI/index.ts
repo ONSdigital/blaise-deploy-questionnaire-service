fe80873eb96684ff238322dabec9037fd5fd80d6
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-07-05 10:48:28 +0100

    Move BIMSAPI Class to its own file

--- server/BimsAPI/index.ts
+++ server/BimsAPI/index.ts
@@ -1,67 +1,7 @@
 import {EnvironmentVariables} from "../Config";
 import express, {Request, Response, Router} from "express";
-import {SendAPIRequest} from "../SendRequest";
-import AuthProvider from "../AuthProvider";
 import {auditLogError, auditLogInfo} from "../audit_logging";
-import * as PinoHttp from "pino-http";
-
-class BimsAPI {
-    private readonly BIMS_API_URL: string;
-    private readonly BIMS_CLIENT_ID: string;
-    private readonly logger: PinoHttp.HttpLogger;
-    private authProvider: AuthProvider;
-
-    constructor(BIMS_API_URL: string, BIMS_CLIENT_ID: string, logger: PinoHttp.HttpLogger) {
-        this.BIMS_API_URL = BIMS_API_URL;
-        this.BIMS_CLIENT_ID = BIMS_CLIENT_ID;
-        this.logger = logger;
-        this.authProvider = new AuthProvider(BIMS_CLIENT_ID);
-    }
-
-    async getStartDate(req: Request, res: Response, instrumentName: string): Promise<[number, any, string]> {
-        const url = `${this.BIMS_API_URL}/tostartdate/${instrumentName}`;
-
-        const authHeader = await this.authProvider.getAuthHeader();
-        req.log.info(authHeader, "Obtained Google auth request header");
-
-        const [status, result, contentType] = await SendAPIRequest(this.logger, req, res, url, "get", null, authHeader);
-
-        return [status, result, contentType];
-    }
-
-    async deleteStartDate(req: Request, res: Response, instrumentName: string): Promise<[number, any, string]> {
-        const url = `${this.BIMS_API_URL}/tostartdate/${instrumentName}`;
-
-        const authHeader = await this.authProvider.getAuthHeader();
-        req.log.info(authHeader, "Obtained Google auth request header");
-
-        const [status, result, contentType] = await SendAPIRequest(this.logger, req, res, url, "DELETE", null, authHeader);
-
-        return [status, result, contentType];
-    }
-
-    async createStartDate(req: Request, res: Response, instrumentName: string, data: any): Promise<[number, any, string]> {
-        const url = `${this.BIMS_API_URL}/tostartdate/${instrumentName}`;
-
-        const authHeader = await this.authProvider.getAuthHeader();
-        req.log.info(authHeader, "Obtained Google auth request header");
-
-        const [status, result, contentType] = await SendAPIRequest(this.logger, req, res, url, "POST", data, authHeader);
-
-        return [status, result, contentType];
-    }
-
-    async updateStartDate(req: Request, res: Response, instrumentName: string, data: any): Promise<[number, any, string]> {
-        const url = `${this.BIMS_API_URL}/tostartdate/${instrumentName}`;
-
-        const authHeader = await this.authProvider.getAuthHeader();
-        req.log.info(authHeader, "Obtained Google auth request header");
-
-        const [status, result, contentType] = await SendAPIRequest(this.logger, req, res, url, "PATCH", data, authHeader);
-
-        return [status, result, contentType];
-    }
-}
+import {BimsAPI} from "./BimsAPI";
 
 export default function BimsAPIRouter(environmentVariables: EnvironmentVariables, logger: any): Router {
     const {BIMS_API_URL, BIMS_CLIENT_ID}: EnvironmentVariables = environmentVariables;
