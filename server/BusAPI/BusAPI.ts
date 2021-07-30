8302df84b5604de858c755f39e88cbc569014886
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-07-30 12:18:33 +0100

    Bus API routes

---
+++ server/BusAPI/BusAPI.ts
@@ -0,0 +1,29 @@
+import * as PinoHttp from "pino-http";
+import AuthProvider from "../AuthProvider";
+import {Request, Response} from "express";
+import {SendAPIRequest} from "../SendRequest";
+
+export class BusAPI {
+    private readonly BUS_API_URL: string;
+    private readonly BUS_CLIENT_ID: string;
+    private readonly logger: PinoHttp.HttpLogger;
+    private authProvider: AuthProvider;
+
+    constructor(BIMS_API_URL: string, BIMS_CLIENT_ID: string, logger: PinoHttp.HttpLogger) {
+        this.BUS_API_URL = BIMS_API_URL;
+        this.BUS_CLIENT_ID = BIMS_CLIENT_ID;
+        this.logger = logger;
+        this.authProvider = new AuthProvider(BIMS_CLIENT_ID);
+    }
+
+    async generateUACsForInstrument(req: Request, res: Response, instrumentName: string): Promise<[number, any, string]> {
+        const url = `${this.BUS_API_URL}/uacs/instrument/${instrumentName}`;
+
+        const authHeader = await this.authProvider.getAuthHeader();
+        req.log.info(authHeader, "Obtained Google auth request header");
+
+        const [status, result, contentType] = await SendAPIRequest(this.logger, req, res, url, "get", null, authHeader);
+
+        return [status, result, contentType];
+    }
+}
---
+++ server/BusAPI/index.ts
@@ -0,0 +1,25 @@
+import {EnvironmentVariables} from "../Config";
+import express, {Request, Response, Router} from "express";
+import {BusAPI} from "./BusAPI";
+
+export default function BusAPIRouter(environmentVariables: EnvironmentVariables, logger: any): Router {
+    const {BUS_API_URL, BUS_CLIENT_ID}: EnvironmentVariables = environmentVariables;
+    const router = express.Router();
+
+    const bimsAPI = new BusAPI(BUS_API_URL, BUS_CLIENT_ID, logger);
+
+    router.post("/api/uacs/instrument/:instrumentName", async function (req: Request, res: Response) {
+        const {instrumentName} = req.params;
+        const [status, result, contentType] = await bimsAPI.generateUACsForInstrument(req, res, instrumentName);
+
+        if (status === 200 && contentType !== "application/json") {
+            req.log.warn("Response was not JSON, most likely invalid auth");
+            res.status(400).json({});
+            return;
+        }
+
+        res.status(status).json(result);
+    });
+
+    return router;
+}
