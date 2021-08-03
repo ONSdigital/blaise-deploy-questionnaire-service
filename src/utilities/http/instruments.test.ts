c4e38805efc14ebbf6bf8659e4c0f5b5e1a1f214
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-08-03 17:38:10 +0100

    instruments.ts UI http request tests

--- src/utilities/http/instruments.test.ts
+++ src/utilities/http/instruments.test.ts
@@ -1,7 +1,13 @@
 import {cleanup} from "@testing-library/react";
 import {mock_server_request_function, mock_server_request_Return_JSON} from "../../tests/utils";
 import {instrumentList, opnInstrument} from "../../features/step_definitions/API_Mock_Objects";
-import {checkInstrumentAlreadyExists, getAllInstruments} from "./instruments";
+import {
+    checkInstrumentAlreadyExists,
+    deleteInstrument,
+    doesInstrumentHaveCAWIMode,
+    getAllInstruments,
+    sendInstallRequest
+} from "./instruments";
 
 describe("Function checkInstrumentAlreadyExists(instrumentName: string) ", () => {
 
@@ -114,6 +120,114 @@ describe("Function getAllInstruments(filename: string) ", () => {
         const [success, instruments] = await getAllInstruments();
         expect(success).toBeFalsy();
         expect(instruments).toEqual([]);
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        cleanup();
+    });
+});
+
+describe("Function deleteInstrument(instrumentName: string) ", () => {
+
+    it("It should return true if created 204 response is returned", async () => {
+        mock_server_request_Return_JSON(204, {});
+        const success = await deleteInstrument("OPN2004A");
+        expect(success).toBeTruthy();
+    });
+
+    it("It should return false if a 404 is returned from the server", async () => {
+        mock_server_request_Return_JSON(404, {});
+        const success = await deleteInstrument("OPN2004A");
+        expect(success).toEqual([false, ""]);
+    });
+
+    it("It should return false if request returns an error code", async () => {
+        mock_server_request_Return_JSON(500, {});
+        const success = await deleteInstrument("OPN2004A");
+        expect(success).toEqual([false, ""]);
+    });
+
+    it("It should return false object if request call fails", async () => {
+        mock_server_request_function(() =>
+            Promise.resolve(() => {
+                throw "error";
+            })
+        );
+        const success = await deleteInstrument("OPN2004A");
+        expect(success).toEqual([false, ""]);
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        cleanup();
+    });
+});
+
+describe("Function sendInstallRequest(instrumentName: string) ", () => {
+
+    it("It should return true if created 201 response is returned", async () => {
+        mock_server_request_Return_JSON(201, {});
+        const success = await sendInstallRequest("OPN2004A");
+        expect(success).toBeTruthy();
+    });
+
+    it("It should return false if a 404 is returned from the server", async () => {
+        mock_server_request_Return_JSON(404, {});
+        const success = await sendInstallRequest("OPN2004A");
+        expect(success).toBeFalsy();
+    });
+
+    it("It should return false if request returns an error code", async () => {
+        mock_server_request_Return_JSON(500, {});
+        const success = await sendInstallRequest("OPN2004A");
+        expect(success).toEqual(false);
+    });
+
+    it("It should return false object if request call fails", async () => {
+        mock_server_request_function(() =>
+            Promise.resolve(() => {
+                throw "error";
+            })
+        );
+        const success = await sendInstallRequest("OPN2004A");
+        expect(success).toEqual(false);
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        cleanup();
+    });
+});
+
+describe("Function doesInstrumentHaveCAWIMode(instrumentName: string) ", () => {
+
+    it("It should return true if created 200 response is returned", async () => {
+        mock_server_request_Return_JSON(200, true);
+        const success = await doesInstrumentHaveCAWIMode("OPN2004A");
+        expect(success).toBeTruthy();
+    });
+
+    it("It should return false if a 404 is returned from the server", async () => {
+        mock_server_request_Return_JSON(404, {});
+        const success = await doesInstrumentHaveCAWIMode("OPN2004A");
+        expect(success).toBeFalsy();
+    });
+
+    it("It should return null if request returns an error code", async () => {
+        mock_server_request_Return_JSON(500, {});
+        const success = await doesInstrumentHaveCAWIMode("OPN2004A");
+        expect(success).toEqual(null);
+    });
+
+    it("It should return null object if request call fails", async () => {
+        mock_server_request_function(() =>
+            Promise.resolve(() => {
+                throw "error";
+            })
+        );
+        const success = await doesInstrumentHaveCAWIMode("OPN2004A");
+        expect(success).toEqual(null);
     });
 
     afterAll(() => {
