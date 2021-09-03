44e31d2dd24cc0479e8ecdad5981822c26d91770
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-09-03 15:55:53 +0100

    Cleanup Request functions and returned promise

--- src/utilities/http/uacCodes.ts
+++ src/utilities/http/uacCodes.ts
@@ -35,18 +35,16 @@ function getCountOfUACs(instrumentName: string): Promise<number | null> {
     console.log(`Sending request to get UAC code count for ${instrumentName}`);
     const url = `/api/uacs/instrument/${instrumentName}/count`;
 
-    return new Promise((resolve: (object: number | null) => void) => {
-        requestPromiseJson("GET", url).then(([status, data]) => {
-            console.log(`Response from get UAC code count: Status ${status}, data ${data}`);
-            if (status === 200 && typeof data.count === "number") {
-                resolve(data.count);
-            } else {
-                resolve(null);
-            }
-        }).catch((error: Error) => {
-            console.error(`Failed to get UAC code count, Error ${error}`);
-            resolve(null);
-        });
+    return requestPromiseJson("GET", url).then(([status, data]): number | null => {
+        console.log(`Response from get UAC code count: Status ${status}, data ${data}`);
+        if (status === 200 && typeof data.count === "number") {
+            return data.count;
+        } else {
+            return null;
+        }
+    }).catch((error: Error) => {
+        console.error(`Failed to get UAC code count, Error ${error}`);
+        return null;
     });
 }
 
