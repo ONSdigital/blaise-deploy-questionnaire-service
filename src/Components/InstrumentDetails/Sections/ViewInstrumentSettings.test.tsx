a4cacae54e71d095174dbe74082106bc7dd76b0c
elthorne <46898565+elthorne@users.noreply.github.com>
2021-11-11 15:11:48 +0000

    tests added

--- src/Components/InstrumentDetails/Sections/ViewInstrumentSettings.test.tsx
+++ src/Components/InstrumentDetails/Sections/ViewInstrumentSettings.test.tsx
@@ -1,4 +1,4 @@
-import flushPromises, {mock_server_request_Return_JSON} from "../../../tests/utils";
+import flushPromises from "../../../tests/utils";
 import {cleanup, render, screen, waitFor} from "@testing-library/react";
 import {act} from "react-dom/test-utils";
 import React from "react";
@@ -6,14 +6,45 @@ import ViewInstrumentSettings from "./ViewInstrumentSettings";
 import {opnInstrument} from "../../../features/step_definitions/API_Mock_Objects";
 import {createMemoryHistory} from "history";
 import {Router} from "react-router";
+import {mock_fetch_requests} from "../../../features/step_definitions/functions";
 
-jest.mock("blaise-api-node-client");
-const {InstrumentSettingsMockList} = jest.requireActual("blaise-api-node-client");
+const viewInstrumentSettingsFailedMessage = /Failed to get questionnaire settings/i;
 
-describe("Given the API successfully loads the instrument mode", () => {
+describe("Given the API successfully loads the instrument mode and settings", () => {
+
+    const InstrumentSettingsMockList = [
+        {
+            "type": "StrictInterviewing",
+            "saveSessionOnTimeout": true,
+            "saveSessionOnQuit": true,
+            "deleteSessionOnTimeout": false,
+            "deleteSessionOnQuit": false,
+            "sessionTimeout": 15,
+            "applyRecordLocking": false
+        }
+    ];
+    const mock_server_responses = (url: string) => {
+        console.log(url);
+        if (url.includes("/modes")) {
+            return Promise.resolve({
+                status: 200,
+                json: () => Promise.resolve(["CATI", "CAWI"]),
+            });
+        }
+        if (url.includes("/settings")) {
+            return Promise.resolve({
+                status: 200,
+                json: () => Promise.resolve(InstrumentSettingsMockList),
+            });
+        }
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve([]),
+        });
+    };
 
     beforeAll(() => {
-        mock_server_request_Return_JSON(200, ["CATI", "CAWI"]);
+        mock_fetch_requests(mock_server_responses);
     });
 
     it("matches Snapshot for the view Instrument Settings page", async () => {
@@ -47,15 +78,13 @@ describe("Given the API successfully loads the instrument mode", () => {
 
         await waitFor(() => {
             expect(screen.getByText(/Questionnaire settings/i)).toBeDefined();
-            // TODO: Helpedy-help
-            // expect(screen.getByText(/Type/i)).toBeDefined();
-            // expect(screen.getByText(/SaveSessionOnTimeout/i)).toBeDefined();
-            // expect(screen.getByText(/SessionTimeout/i)).toBeDefined();
+            expect(screen.getByText(/Type/i)).toBeDefined();
+            expect(screen.getByText(/SaveSessionOnTimeout/i)).toBeDefined();
+            expect(screen.getByText(/SessionTimeout/i)).toBeDefined();
         });
     });
 
     it("should highlight all invalid fields", async () => {
-        // const instrumentList: any[] = [{name: "OPN2101A"}, {name: "OPN2004A"}, {name: "LMS2101_BK2"}];
         const history = createMemoryHistory();
         render(
             <Router history={history}>
@@ -68,10 +97,9 @@ describe("Given the API successfully loads the instrument mode", () => {
         });
 
         await waitFor(() => {
-            // TODO: Helpedy-help
-            // expect(screen.getByText(/DeleteSessionOnTimeout should be True/i)).toBeDefined();
-            // expect(screen.getByText(/DeleteSessionOnQuit should be True/i)).toBeDefined();
-            // expect(screen.getByText(/ApplyRecordLocking should be True/i)).toBeDefined();
+            expect(screen.getByText(/DeleteSessionOnTimeout should be True/i)).toBeDefined();
+            expect(screen.getByText(/DeleteSessionOnQuit should be True/i)).toBeDefined();
+            expect(screen.getByText(/ApplyRecordLocking should be True/i)).toBeDefined();
         });
     });
 
@@ -81,82 +109,30 @@ describe("Given the API successfully loads the instrument mode", () => {
     });
 });
 
-describe("Given the API successfully loads the instrument settings", () => {
+describe("Given the API fails to load the instrument mode and settings", () => {
+
+    const mock_server_responses = (url: string) => {
+        console.log(url);
+        if (url.includes("/modes")) {
+            return Promise.resolve({
+                status: 500,
+                json: () => Promise.resolve({}),
+            });
+        }
+        if (url.includes("/settings")) {
+            return Promise.resolve({
+                status: 500,
+                json: () => Promise.resolve({}),
+            });
+        }
+        return Promise.resolve({
+            status: 500,
+            json: () => Promise.resolve({}),
+        });
+    };
 
     beforeAll(() => {
-        mock_server_request_Return_JSON(200, InstrumentSettingsMockList);
-    });
-
-    it("matches Snapshot for the view Instrument Settings page", async () => {
-        const history = createMemoryHistory();
-        const wrapper = render(
-            <Router history={history}>
-                <ViewInstrumentSettings instrument={opnInstrument}/>
-            </Router>
-        );
-
-        await act(async () => {
-            await flushPromises();
-        });
-
-        await waitFor(() => {
-            expect(wrapper).toMatchSnapshot();
-        });
-    });
-
-    it("should render correctly", async () => {
-        const history = createMemoryHistory();
-        render(
-            <Router history={history}>
-                <ViewInstrumentSettings instrument={opnInstrument}/>
-            </Router>
-        );
-
-        await act(async () => {
-            await flushPromises();
-        });
-
-        await waitFor(() => {
-            expect(screen.getByText(/Questionnaire settings/i)).toBeDefined();
-            // TODO: Helpedy-help
-            // expect(screen.getByText(/Type/i)).toBeDefined();
-            // expect(screen.getByText(/SaveSessionOnTimeout/i)).toBeDefined();
-            // expect(screen.getByText(/SessionTimeout/i)).toBeDefined();
-        });
-    });
-
-    it("should highlight all invalid fields", async () => {
-        // const instrumentList: any[] = [{name: "OPN2101A"}, {name: "OPN2004A"}, {name: "LMS2101_BK2"}];
-        const history = createMemoryHistory();
-        render(
-            <Router history={history}>
-                <ViewInstrumentSettings instrument={opnInstrument}/>
-            </Router>
-        );
-
-        await act(async () => {
-            await flushPromises();
-        });
-
-        await waitFor(() => {
-            // TODO: Helpedy-help
-            // expect(screen.getByText(/DeleteSessionOnTimeout should be True/i)).toBeDefined();
-            // expect(screen.getByText(/DeleteSessionOnQuit should be True/i)).toBeDefined();
-            // expect(screen.getByText(/ApplyRecordLocking should be True/i)).toBeDefined();
-        });
-    });
-
-    afterAll(() => {
-        jest.clearAllMocks();
-        cleanup();
-    });
-});
-
-describe("Given the API fails to load the instrument mode and/or settings", () => {
-
-    const viewInstrumentSettingsFailedMessage = /Failed to get questionnaire settings/i;
-    beforeAll(() => {
-        mock_server_request_Return_JSON(500, {});
+        mock_fetch_requests(mock_server_responses);
     });
 
     it("should display an error message when it fails to load the Instrument Modes", async () => {
@@ -185,15 +161,34 @@ describe("Given the API fails to load the instrument mode and/or settings", () =
 
         await waitFor(() => {
             expect(screen.getByText(viewInstrumentSettingsFailedMessage)).toBeDefined();
-            expect(screen.getByText(/Failed to get questionnaire settings/i)).toBeDefined();
         });
     });
 });
 
 describe("Given the API returns an empty list for instrument mode and/or settings", () => {
 
+    const mock_server_responses = (url: string) => {
+        console.log(url);
+        if (url.includes("/modes")) {
+            return Promise.resolve({
+                status: 200,
+                json: () => Promise.resolve([]),
+            });
+        }
+        if (url.includes("/settings")) {
+            return Promise.resolve({
+                status: 200,
+                json: () => Promise.resolve([]),
+            });
+        }
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve([]),
+        });
+    };
+
     beforeAll(() => {
-        mock_server_request_Return_JSON(200, []);
+        mock_fetch_requests(mock_server_responses);
     });
 
     it("it should render an error message", async () => {
@@ -209,7 +204,7 @@ describe("Given the API returns an empty list for instrument mode and/or setting
         });
 
         await waitFor(() => {
-            expect(screen.getByText(/Failed to get questionnaire settings/i)).toBeDefined();
+            expect(screen.getByText(viewInstrumentSettingsFailedMessage)).toBeDefined();
         });
     });
 
@@ -218,4 +213,3 @@ describe("Given the API returns an empty list for instrument mode and/or setting
         cleanup();
     });
 });
-
