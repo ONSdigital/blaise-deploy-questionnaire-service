099657063baba75f064194c5e9772d4332baf26a
elthorne <46898565+elthorne@users.noreply.github.com>
2021-11-12 13:00:57 +0000

    Added test for CATI only mode

--- src/Components/InstrumentDetails/Sections/ViewInstrumentSettings.test.tsx
+++ src/Components/InstrumentDetails/Sections/ViewInstrumentSettings.test.tsx
@@ -9,20 +9,107 @@ import {Router} from "react-router";
 import {mock_fetch_requests} from "../../../features/step_definitions/functions";
 
 const viewInstrumentSettingsFailedMessage = /Failed to get questionnaire settings/i;
+const InstrumentSettingsMockList = [
+    {
+        "type": "StrictInterviewing",
+        "saveSessionOnTimeout": false,
+        "saveSessionOnQuit": true,
+        "deleteSessionOnTimeout": false,
+        "deleteSessionOnQuit": false,
+        "sessionTimeout": 15,
+        "applyRecordLocking": false
+    }
+];
 
-describe("Given the API successfully loads the instrument mode and settings", () => {
 
-    const InstrumentSettingsMockList = [
-        {
-            "type": "StrictInterviewing",
-            "saveSessionOnTimeout": true,
-            "saveSessionOnQuit": true,
-            "deleteSessionOnTimeout": false,
-            "deleteSessionOnQuit": false,
-            "sessionTimeout": 15,
-            "applyRecordLocking": false
+describe("Given the API successfully loads the instrument mode and settings for CATI only mode", () => {
+
+    const mock_server_responses = (url: string) => {
+        console.log(url);
+        if (url.includes("/modes")) {
+            return Promise.resolve({
+                status: 200,
+                json: () => Promise.resolve(["CATI"]),
+            });
         }
-    ];
+        if (url.includes("/settings")) {
+            return Promise.resolve({
+                status: 200,
+                json: () => Promise.resolve(InstrumentSettingsMockList),
+            });
+        }
+        return Promise.resolve({
+            status: 200,
+            json: () => Promise.resolve([]),
+        });
+    };
+
+    beforeAll(() => {
+        mock_fetch_requests(mock_server_responses);
+    });
+
+    it("matches Snapshot for the view Instrument Settings page", async () => {
+        const history = createMemoryHistory();
+        const wrapper = render(
+            <Router history={history}>
+                <ViewInstrumentSettings instrument={opnInstrument}/>
+            </Router>
+        );
+
+        await act(async () => {
+            await flushPromises();
+        });
+
+        await waitFor(() => {
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    it("should render correctly", async () => {
+        const history = createMemoryHistory();
+        render(
+            <Router history={history}>
+                <ViewInstrumentSettings instrument={opnInstrument}/>
+            </Router>
+        );
+
+        await act(async () => {
+            await flushPromises();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText(/Questionnaire settings/i)).toBeDefined();
+            expect(screen.getByText(/Type/i)).toBeDefined();
+            expect(screen.getByText(/SaveSessionOnQuit/i)).toBeDefined();
+            expect(screen.getByText(/SessionTimeout/i)).toBeDefined();
+        });
+    });
+
+    it("should highlight all invalid fields", async () => {
+        const history = createMemoryHistory();
+        render(
+            <Router history={history}>
+                <ViewInstrumentSettings instrument={opnInstrument}/>
+            </Router>
+        );
+
+        await act(async () => {
+            await flushPromises();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText(/SaveSessionOnTimeout should be True/i)).toBeDefined();
+        });
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+        cleanup();
+    });
+});
+
+describe("Given the API successfully loads the instrument mode and settings for mixed mode", () => {
+
     const mock_server_responses = (url: string) => {
         console.log(url);
         if (url.includes("/modes")) {
@@ -79,7 +166,7 @@ describe("Given the API successfully loads the instrument mode and settings", ()
         await waitFor(() => {
             expect(screen.getByText(/Questionnaire settings/i)).toBeDefined();
             expect(screen.getByText(/Type/i)).toBeDefined();
-            expect(screen.getByText(/SaveSessionOnTimeout/i)).toBeDefined();
+            expect(screen.getByText(/SaveSessionOnQuit/i)).toBeDefined();
             expect(screen.getByText(/SessionTimeout/i)).toBeDefined();
         });
     });
@@ -97,6 +184,7 @@ describe("Given the API successfully loads the instrument mode and settings", ()
         });
 
         await waitFor(() => {
+            expect(screen.getByText(/SaveSessionOnTimeout should be True/i)).toBeDefined();
             expect(screen.getByText(/DeleteSessionOnTimeout should be True/i)).toBeDefined();
             expect(screen.getByText(/DeleteSessionOnQuit should be True/i)).toBeDefined();
             expect(screen.getByText(/ApplyRecordLocking should be True/i)).toBeDefined();
