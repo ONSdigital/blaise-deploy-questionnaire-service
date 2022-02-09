cc302c57916ca40ab4348bca7ecb8093971aa1fa
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-02-09 17:11:46 +0000

    refactor: place error in a panel at top of the page
    reviewed-by: James & Rich
    Refs: BLAIS5-2999

--- src/Components/UploadPage/Sections/InvalidSettings.tsx
+++ src/Components/UploadPage/Sections/InvalidSettings.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import InstrumentSettingsTable from "../../InstrumentSettings/InstrumentSettingsTable";
 import { InstrumentSettings } from "blaise-api-node-client";
+import {ONSPanel} from "blaise-design-system-react-components";
 
 interface SelectFilePageProps {
   instrumentName: string;
@@ -16,16 +17,15 @@ function ConfirmOverride({ instrumentName, instrumentSettings, invalidSettings, 
       <h1 className="u-mb-l">
         Questionnaire settings for {instrumentName} do not match recommendations
       </h1>
-
-      <InstrumentSettingsTable instrumentSettings={instrumentSettings} invalidSettings={invalidSettings} errored={errored} />
-
-      <p>
+      <ONSPanel>
         This questionnaire does not conform to the standard settings.
 
         If this is intended, please click the &quotDeploy anyway&quot button. If it was not intended, please select the &quotCancel&quot button, and start again.
 
         Do not navigate away from this page without selecting an option.
-      </p>
+      </ONSPanel>
+
+      <InstrumentSettingsTable instrumentSettings={instrumentSettings} invalidSettings={invalidSettings} errored={errored} />
     </>
   );
 }
