d3b67ae3b933a37317694de30302378097b578a6
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-02-09 15:37:05 +0000

    refactor:  add semicolon to env file
    Passes unit test but React does not render correctly.
    
    reviewed-by: James & Sam
    Refs: BLAIS5-3017

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -1,3 +1,3 @@
 export function isProduction(hostname: string): boolean {
-    return hostname.endsWith(".blaise.gcp.onsdigital.uk")
-};
+    return hostname.endsWith(".blaise.gcp.onsdigital.uk");
+}
