59ee85f048a693cee6e3ae8b53177c878bbfbba0
elthorne <46898565+elthorne@users.noreply.github.com>
2022-03-14 16:33:44 +0000

    refactor: Swap out Cati endpoint for generic instrument endpoint.
    Messed about with year calendar tests.
    
    Reviewed-by: El
    Refs: BLAIS5-3031

--- src/components/instrumentDetails/sections/yearCalendar.test.tsx
+++ src/components/instrumentDetails/sections/yearCalendar.test.tsx
@@ -9,68 +9,7 @@ import flushPromises from "../../../tests/utils";
 import YearCalendar from "./yearCalendar";
 import userEvent from "@testing-library/user-event";
 
-describe("Year calendar for CATI mode", () => {
-    afterEach(() => {
-        cleanup();
-    });
-
-    const surveyDays = [
-        "24 Dec 1997 00:00:00 GMT",
-        "20 Dec 1997 00:00:00 GMT"
-    ];
-
-    const modes = [
-        "CATI"
-    ]
-
-    it("should render calendar with the year set as the newest date in survey list specified ", async () => {
-        render(
-            <YearCalendar modes={modes} surveyDays={surveyDays} />
-        );
-
-        await act(async () => {
-            await flushPromises();
-        });
-
-        await waitFor(() => {
-            expect(screen.getByText(/1997/i)).toBeDefined();
-        });
-    });
-
-    it("should go back a year when you press the back button («)", async () => {
-        render(
-            <YearCalendar modes={modes} surveyDays={surveyDays} />
-        );
-
-        await act(async () => {
-            await flushPromises();
-        });
-
-        userEvent.click(screen.getByText("«"));
-
-        await waitFor(() => {
-            expect(screen.getByText(/1996/i)).toBeDefined();
-        });
-    });
-
-    it("should go forward a year when you press the forward button (»)", async () => {
-        render(
-            <YearCalendar modes={modes} surveyDays={surveyDays} />
-        );
-
-        await act(async () => {
-            await flushPromises();
-        });
-
-        userEvent.click(screen.getByText("»"));
-
-        await waitFor(() => {
-            expect(screen.getByText(/1998/i)).toBeDefined();
-        });
-    });
-});
-
-describe("Year calendar for mixed CATI mode", () => {
+describe("Year calendar for mixed and CATI mode", () => {
     afterEach(() => {
         cleanup();
     });
