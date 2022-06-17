c7a36f3bcdcdd301803cc14fd21a16c70c0eb087
elthorne <46898565+elthorne@users.noreply.github.com>
2022-06-17 16:08:24 +0100

    wip: Deploy to sandbox.
    I'm so sorry, I can't reach this page locally. It has to be deployed to my sandbox for my sanity check.
    
    reviewed-by: El
    refs: BLAIS5-3139

--- src/components/uploadPage/sections/deployFormSummary.tsx
+++ src/components/uploadPage/sections/deployFormSummary.tsx
@@ -1,16 +1,35 @@
-import React, { ReactElement } from "react";
-import { FormikContextType, useFormikContext } from "formik";
-import { Questionnaire } from "blaise-api-node-client";
+import React, {ReactElement} from "react";
+import {FormikContextType, useFormikContext} from "formik";
+import {Questionnaire} from "blaise-api-node-client";
 import dateFormatter from "dayjs";
-import { roundUp } from "../../../utilities/maths";
+import {roundUp} from "../../../utilities/maths";
 
 interface PageFourProps {
     file: File | undefined
     foundQuestionnaire: Questionnaire | null
 }
 
-function DeployFormSummary({ file, foundQuestionnaire }: PageFourProps): ReactElement {
-    const { values: formValues }: FormikContextType<any> = useFormikContext();
+function DeployFormSummary({file, foundQuestionnaire}: PageFourProps): ReactElement {
+    const {values: formValues}: FormikContextType<any> = useFormikContext();
+
+    function QuestionnaireFileName(): ReactElement {
+        return (
+            <>
+                <tbody className="summary__item">
+                    <tr className="summary__row summary__row--has-values">
+                        <td className="summary__item-title">
+                            <div className="summary__item--text">
+                                Questionnaire file name
+                            </div>
+                        </td>
+                        <td className="summary__values">
+                            {file?.name}
+                        </td>
+                    </tr>
+                </tbody>
+            </>
+        );
+    }
 
     return (
         <>
@@ -19,80 +38,69 @@ function DeployFormSummary({ file, foundQuestionnaire }: PageFourProps): ReactEl
                     <h1 className="summary__group-title">Deployment summary</h1>
                     <table className="summary__items">
                         <thead className="u-vh">
-                            <tr>
-                                <th>Question</th>
-                                <th>Answer given</th>
-                            </tr>
+                        <tr>
+                            <th>Question</th>
+                            <th>Answer given</th>
+                        </tr>
                         </thead>
+                        <QuestionnaireFileName/>
                         <tbody className="summary__item">
-                            <tr className="summary__row summary__row--has-values">
-                                <td className="summary__item-title">
-                                    <div className="summary__item--text">
-                                        Questionnaire file name
-                                    </div>
-                                </td>
-                                <td className="summary__values">
-                                    {file?.name}
-                                </td>
-                            </tr>
+                        <tr className="summary__row summary__row--has-values">
+                            <td className="summary__item-title">
+                                <div className="summary__item--text">
+                                    Questionnaire file last modified date
+                                </div>
+                            </td>
+                            <td className="summary__values">
+                                {dateFormatter(file?.lastModified).format("DD/MM/YYYY HH:MM")}
+                            </td>
+                        </tr>
                         </tbody>
                         <tbody className="summary__item">
-                            <tr className="summary__row summary__row--has-values">
-                                <td className="summary__item-title">
-                                    <div className="summary__item--text">
-                                        Questionnaire file last modified date
-                                    </div>
-                                </td>
-                                <td className="summary__values">
-                                    {dateFormatter(file?.lastModified).format("DD/MM/YYYY HH:MM")}
-                                </td>
-                            </tr>
+                        <tr className="summary__row summary__row--has-values">
+                            <td className="summary__item-title">
+                                <div className="summary__item--text">
+                                    Questionnaire file size
+                                </div>
+                            </td>
+                            <td className="summary__values">
+                                {(file && roundUp(file.size / 1000000, 0))}MB
+                            </td>
+                        </tr>
                         </tbody>
                         <tbody className="summary__item">
-                            <tr className="summary__row summary__row--has-values">
-                                <td className="summary__item-title">
-                                    <div className="summary__item--text">
-                                        Questionnaire file size
-                                    </div>
-                                </td>
-                                <td className="summary__values">
-                                    {(file && roundUp(file.size / 1000000, 0))}MB
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tbody className="summary__item">
-                            <tr className="summary__row summary__row--has-values">
-                                <td className="summary__item-title">
-                                    <div className="summary__item--text">
-                                        Does the questionnaire already exist in blaise?
-                                    </div>
-                                </td>
-                                <td className="summary__values">
-                                    {
-                                        foundQuestionnaire ?
-                                            "Yes, overriding questionnaire" : "No"
-                                    }
-                                </td>
-                            </tr>
+                        <tr className="summary__row summary__row--has-values">
+                            <td className="summary__item-title">
+                                <div className="summary__item--text">
+                                    Does the questionnaire already exist in blaise?
+                                </div>
+                            </td>
+                            <td className="summary__values">
+                                {
+                                    foundQuestionnaire ?
+                                        "Yes, overriding questionnaire" : "No"
+                                }
+                            </td>
+                        </tr>
                         </tbody>
 
                         <tbody className="summary__item">
-                            <tr className="summary__row summary__row--has-values">
-                                <td className="summary__item-title">
-                                    <div className="summary__item--text">
-                                        Set a telephone operations start date for questionnaire?
-                                    </div>
-                                </td>
-                                <td className="summary__values">
-                                    {
-                                        formValues["set start date"] ?
-                                            `Start date set to ${dateFormatter(formValues["set start date"]).format("DD/MM/YYYY")}`
-                                            :
-                                            "Start date not specified"
-                                    }
+                        <tr className="summary__row summary__row--has-values">
+                            <td className="summary__item-title">
+                                <div className="summary__item--text">
+                                    Set a telephone operations start date for questionnaire?
+                                </div>
+                            </td>
+                            <td className="summary__values">
+                                {
+                                    formValues["set start date"] ?
+                                        `Start date set to ${dateFormatter(formValues["set start date"]).format("DD/MM/YYYY")}`
+                                        :
+                                        "Start date not specified"
+                                }
 
-                                </td>
-                            </tr>
+                            </td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
