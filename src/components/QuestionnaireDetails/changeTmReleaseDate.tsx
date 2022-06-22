72d7fc502319351fa537680c08a1f09994eaf437
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-06-22 09:41:38 +0100

    feat: add change TM release date functionality
    Reviewed-by: James & :squirrel
    Refs: BLAIS5-3141

---
+++ src/components/QuestionnaireDetails/changeTmReleaseDate.tsx
@@ -0,0 +1,86 @@
+import React, { ReactElement } from "react";
+import { ONSButton } from "blaise-design-system-react-components";
+import { setTMReleaseDate } from "../../client/tmReleaseDate";
+import dateFormatter from "dayjs";
+import { useHistory, useLocation } from "react-router-dom";
+import { Form, Formik } from "formik";
+import AskToSetTMReleaseDate from "../uploadPage/sections/askToSetTMReleaseDate";
+import Breadcrumbs from "../breadcrumbs";
+
+interface LocationState {
+    tmReleaseDate: string | null;
+    questionnaireName: string;
+}
+
+function ChangeTMReleaseDate(): ReactElement {
+    const history = useHistory();
+    const location = useLocation<LocationState>();
+    const { tmReleaseDate, questionnaireName } = location.state || { tmReleaseDate: null, questionnaireName: null };
+
+    async function _handleSubmit(values: any, actions: any) {
+        if (values.askToSetDate === "no") {
+            values["set start date"] = "";
+        }
+        const liveDateCreated = await setTMReleaseDate(questionnaireName, values["set release date"]);
+        if (!liveDateCreated) {
+            console.log("Failed to store Totalmobile release date specified");
+            return;
+        }
+        actions.setSubmitting(false);
+        history.goBack();
+    }
+
+    let initialValues = {
+        askToSetDate: "",
+        "set release date": ""
+    };
+
+    if (tmReleaseDate != null) {
+        initialValues = {
+            askToSetDate: "yes",
+            "set release date": dateFormatter(tmReleaseDate).format("YYYY-MM-DD")
+        };
+    }
+
+    return (
+        <>
+            <Breadcrumbs BreadcrumbList={
+                [
+                    { link: "/", title: "Home" }, { link: `/questionnaire/${questionnaireName}`, title: questionnaireName }
+                ]
+            } />
+
+            <main id="main-content" className="page__main u-mt-no">
+                <Formik
+                    validateOnBlur={false}
+                    validateOnChange={false}
+                    initialValues={initialValues}
+                    onSubmit={_handleSubmit}
+                >
+                    {({ isSubmitting }) => (
+                        <Form id={"formID"}>
+                            <AskToSetTMReleaseDate questionnaireName={questionnaireName} />
+
+                            <div className="btn-group u-mt-m">
+                                <ONSButton
+                                    id={"continue-deploy-button"}
+                                    submit={true}
+                                    loading={isSubmitting}
+                                    primary={true} label={"Continue"} />
+                                {!isSubmitting && (
+                                    <ONSButton
+                                        id={"cancel-deploy-button"}
+                                        onClick={() => history.goBack()}
+                                        primary={false} label={"Cancel"} />
+                                )}
+                            </div>
+
+                        </Form>
+                    )}
+                </Formik>
+            </main>
+        </>
+    );
+}
+
+export default ChangeTMReleaseDate;
