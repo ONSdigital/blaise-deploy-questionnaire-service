2c90637ef1eb7eb2f916223b55ed0a831b0be8a2
Tom Oram <tom.oram@armakuni.com>
2022-08-31 16:13:08 +0100

    refactor: Tidy logging code
    Refs: BLAIS5-3166
    Co-authored-by: elthorne <elthorne@users.noreply.github.com>

--- src/server/bimsApi/loggingReleaseDateManager.ts
+++ src/server/bimsApi/loggingReleaseDateManager.ts
@@ -13,19 +13,17 @@ export default class LoggingReleaseDateManager implements ReleaseDateManager {
         return this.performActionAndLog(
             () => this.instance.createReleaseDate(questionnaireName, releaseDate),
             `Totalmobile release date set to ${releaseDate} for ${questionnaireName} by ${this.username}`,
-            `Failed to set TM release date to ${releaseDate} for questionnaire ${questionnaireName} (user: ${this.username})`
+            `Failed to set TM release date to ${releaseDate} for questionnaire ${questionnaireName} (user: ${this.username})`,
+            questionnaireName
         );
     }
 
     deleteReleaseDate(questionnaireName: string): Promise<void> {
-        let successMessage = `Totalmobile release date deleted for ${questionnaireName} by ${this.username}`;
-        if(this.previousReleaseDate.has(questionnaireName)) {
-            successMessage = `Totalmobile release date deleted (previously ${dateFormatter(this.previousReleaseDate.get(questionnaireName)).format("YYYY-MM-DD")}) for ${questionnaireName} by ${this.username}`;
-        }
         return this.performActionAndLog(
             () => this.instance.deleteReleaseDate(questionnaireName),
-            successMessage,
-            `Failed to remove TM release date for questionnaire ${questionnaireName} (user: ${this.username})`
+            `Totalmobile release date deleted{previous} for ${questionnaireName} by ${this.username}`,
+            `Failed to remove TM release date for questionnaire ${questionnaireName} (user: ${this.username})`,
+            questionnaireName
         );
     }
 
@@ -39,25 +37,30 @@ export default class LoggingReleaseDateManager implements ReleaseDateManager {
     }
 
     updateReleaseDate(questionnaireName: string, releaseDate: string): Promise<tmReleaseDate> {
-        let successMessage = `Totalmobile release date updated to ${releaseDate} for ${questionnaireName} by ${this.username}`;
-        if(this.previousReleaseDate.has(questionnaireName)){
-            successMessage = `Totalmobile release date updated to ${releaseDate} (previously ${dateFormatter(this.previousReleaseDate.get(questionnaireName)).format("YYYY-MM-DD")}) for ${questionnaireName} by ${this.username}`;
-        }
-
         return this.performActionAndLog(
             () => this.instance.updateReleaseDate(questionnaireName, releaseDate),
-            successMessage,
-            `Failed to set TM release date to ${releaseDate} for questionnaire ${questionnaireName} (user: ${this.username})`
+            `Totalmobile release date updated to ${releaseDate}{previous} for ${questionnaireName} by ${this.username}`,
+            `Failed to set TM release date to ${releaseDate} for questionnaire ${questionnaireName} (user: ${this.username})`,
+            questionnaireName
         );
     }
 
-    private async performActionAndLog <Result> (action: () => Promise<Result>, successMessage: string, errorMessage: string): Promise<Result> {
+    private async performActionAndLog <Result>(action: () => Promise<Result>, successMessage: string, errorMessage: string, questionnaireName: string): Promise<Result> {
+        const prepareMessage = (successMessage: string) => {
+            let previous = "";
+            if (this.previousReleaseDate.has(questionnaireName)) {
+                previous = ` (previously ${dateFormatter(this.previousReleaseDate.get(questionnaireName)).format("YYYY-MM-DD")})`;
+            }
+            return successMessage
+                .replace("{previous}", previous);
+        };
+
         try {
             const result = await action();
-            this.logger.info(successMessage);
+            this.logger.info(prepareMessage(successMessage));
             return result;
         } catch (error) {
-            this.logger.error(errorMessage);
+            this.logger.error(prepareMessage(errorMessage));
             throw error;
         }
     }
