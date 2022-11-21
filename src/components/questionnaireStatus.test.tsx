bfa946a21f03e27c4cf6306f1509cb182610cfa0
enigma-cloud <57638182+Enigma-cloud@users.noreply.github.com>
2022-11-21 16:42:31 +0000

    test: update css classes

--- src/components/questionnaireStatus.test.tsx
+++ src/components/questionnaireStatus.test.tsx
@@ -12,35 +12,35 @@ describe("Questionnaire Status ", () => {
         cleanup();
     });
 
-    it("should render questionnaire with class 'status--success' when the status is Active", async () => {
+    it("should render questionnaire with class 'ons-status--success' when the status is Active", async () => {
         const { container } = render(
             <QuestionnaireStatus status={"Active"} />
         );
 
-        expect(container.firstChild).toHaveClass("status--success");
+        expect(container.firstChild).toHaveClass("ons-status--success");
     });
 
-    it("should render questionnaire with class 'status--error' when the status is Erroneous", async () => {
+    it("should render questionnaire with class 'ons-status--error' when the status is Erroneous", async () => {
         const { container } = render(
             <QuestionnaireStatus status={"Failed"} />
         );
 
-        expect(container.firstChild).toHaveClass("status--error");
+        expect(container.firstChild).toHaveClass("ons-status--error");
     });
 
-    it("should render questionnaire with class 'status--error' when the status is Failed", async () => {
+    it("should render questionnaire with class 'ons-status--error' when the status is Failed", async () => {
         const { container } = render(
             <QuestionnaireStatus status={"Failed"} />
         );
 
-        expect(container.firstChild).toHaveClass("status--error");
+        expect(container.firstChild).toHaveClass("ons-status--error");
     });
 
-    it("should render questionnaire with class 'status--info' when the status is not known", async () => {
+    it("should render questionnaire with class 'ons-status--info' when the status is not known", async () => {
         const { container } = render(
             <QuestionnaireStatus status={"Bacon"} />
         );
 
-        expect(container.firstChild).toHaveClass("status--info");
+        expect(container.firstChild).toHaveClass("ons-status--info");
     });
 });
