8b47f638cdf066e4eff82a988d7c0921247206e0
elthorne <46898565+elthorne@users.noreply.github.com>
2023-03-14 14:09:10 +0000

    Refactor: yarn lint --fix
    Refs: BLAIS5-3651

--- src/components/questionnaireDetailsPage/sections/questionnaireDetails.tsx
+++ src/components/questionnaireDetailsPage/sections/questionnaireDetails.tsx
@@ -16,58 +16,58 @@ function QuestionnaireDetails({ questionnaire, modes }: Props): ReactElement {
                     <h2 className="ons-summary__group-title">Questionnaire details</h2>
                     <table className="ons-summary__items">
                         <thead className="ons-u-vh">
-                        <tr>
-                            <th>Detail</th>
-                            <th>Output</th>
-                        </tr>
+                            <tr>
+                                <th>Detail</th>
+                                <th>Output</th>
+                            </tr>
                         </thead>
                         <tbody className="ons-summary__item">
-                        <tr className="ons-summary__row ons-summary__row--has-values">
-                            <td className="ons-summary__item-title">
-                                <div className="ons-summary__item--text">
+                            <tr className="ons-summary__row ons-summary__row--has-values">
+                                <td className="ons-summary__item-title">
+                                    <div className="ons-summary__item--text">
                                     Questionnaire status
-                                </div>
-                            </td>
-                            <td className="ons-summary__values" colSpan={2}>
-                                <QuestionnaireStatus status={questionnaire.status ? questionnaire.status : ""}/>
-                            </td>
-                        </tr>
+                                    </div>
+                                </td>
+                                <td className="ons-summary__values" colSpan={2}>
+                                    <QuestionnaireStatus status={questionnaire.status ? questionnaire.status : ""}/>
+                                </td>
+                            </tr>
                         </tbody>
                         <tbody className="ons-summary__item">
-                        <tr className="ons-summary__row ons-summary__row--has-values">
-                            <td className="ons-summary__item-title">
-                                <div className="ons-summary__item--text">
+                            <tr className="ons-summary__row ons-summary__row--has-values">
+                                <td className="ons-summary__item-title">
+                                    <div className="ons-summary__item--text">
                                     Modes
-                                </div>
-                            </td>
-                            <td className="ons-summary__values" colSpan={2}>
-                                {modes.join(", ")}
-                            </td>
-                        </tr>
+                                    </div>
+                                </td>
+                                <td className="ons-summary__values" colSpan={2}>
+                                    {modes.join(", ")}
+                                </td>
+                            </tr>
                         </tbody>
                         <tbody className="ons-summary__item">
-                        <tr className="ons-summary__row ons-summary__row--has-values">
-                            <td className="ons-summary__item-title">
-                                <div className="ons-summary__item--text">
+                            <tr className="ons-summary__row ons-summary__row--has-values">
+                                <td className="ons-summary__item-title">
+                                    <div className="ons-summary__item--text">
                                     Number of cases
-                                </div>
-                            </td>
-                            <td className="ons-summary__values" colSpan={2}>
-                                {questionnaire.dataRecordCount}
-                            </td>
-                        </tr>
+                                    </div>
+                                </td>
+                                <td className="ons-summary__values" colSpan={2}>
+                                    {questionnaire.dataRecordCount}
+                                </td>
+                            </tr>
                         </tbody>
                         <tbody className="ons-summary__item">
-                        <tr className="ons-summary__row ons-summary__row--has-values">
-                            <td className="ons-summary__item-title">
-                                <div className="ons-summary__item--text">
+                            <tr className="ons-summary__row ons-summary__row--has-values">
+                                <td className="ons-summary__item-title">
+                                    <div className="ons-summary__item--text">
                                     Install date
-                                </div>
-                            </td>
-                            <td className="ons-summary__values" colSpan={2}>
-                                {dateFormatter(questionnaire.installDate).format("DD/MM/YYYY HH:mm")}
-                            </td>
-                        </tr>
+                                    </div>
+                                </td>
+                                <td className="ons-summary__values" colSpan={2}>
+                                    {dateFormatter(questionnaire.installDate).format("DD/MM/YYYY HH:mm")}
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
