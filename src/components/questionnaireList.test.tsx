26a0874f2f071922125ada81f96ac43e41cad593
SidraJaved <sidrajaved92@gmail.com>
2024-02-13 13:07:03 +0000

    test-fix: refactored the unit test for questionnaireList component
    Refs: BLAIS5-3719

--- src/components/questionnaireList.test.tsx
+++ src/components/questionnaireList.test.tsx
@@ -3,15 +3,15 @@
  */
 
 import flushPromises from "../tests/utils";
-import { questionnaireList } from "../features/step_definitions/helpers/apiMockObjects";
 import { render, waitFor, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import App from "../app";
+import { Router } from "react-router-dom";
 import { act } from "react-dom/test-utils";
 import React from "react";
 import { AuthManager } from "blaise-login-react-client";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import { createMemoryHistory } from "history";
+import QuestionnaireList from "./questionnaireList";
 
 const mock = new MockAdapter(axios);
 
@@ -20,9 +20,16 @@ AuthManager.prototype.loggedIn = jest.fn().mockImplementation(() => {
     return Promise.resolve(true);
 });
 
+jest.mock("../client/questionnaires");
+import { getQuestionnaires } from "../client/questionnaires";
+import { Questionnaire } from "blaise-api-node-client";
+
+
+const getQuestionnairesMock = getQuestionnaires as jest.Mock<Promise<Questionnaire[]>>;
+
 describe("Questionnaire Details page ", () => {
     beforeEach(() => {
-        mock.onGet("/api/questionnaires").reply(200, questionnaireList);
+        getQuestionnairesMock.mockImplementation(() => Promise.resolve([]));
     });
 
     afterEach(() => {
@@ -30,25 +37,22 @@ describe("Questionnaire Details page ", () => {
     });
 
     it("should redirect to the homepage when no questionnaire has been provided ", async () => {
-        //const history = createMemoryHistory();
-        // Go direct to the questionnaire details page not from a link
-        //history.push("/questionnaire");
-
-        // const history = createMemoryHistory();
-        // const route = "/questionnaire";
-        // history.push(route);
-
+        const history = createMemoryHistory();
+        const route = "/questionnaire";
+        history.push(route);
         render(
-            <MemoryRouter>
-                <App />
-            </MemoryRouter>
+            <Router location={history.location} navigator={history} >
+                <QuestionnaireList setErrored={jest.fn()} />
+            </Router >
         );
+
 
         await act(async () => {
             await flushPromises();
         });
 
         await waitFor(() => {
+            expect(screen.queryByText("Questionnaire settings")).toEqual(null);
             expect(screen.getByText(/Filter by questionnaire name/i)).toBeDefined();
             expect(screen.queryByText(/Questionnaire details/i)).toEqual(null);
         });
