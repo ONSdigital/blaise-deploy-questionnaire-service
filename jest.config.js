ba95a1a7c98382c08ca50d3ad6dceaccc3665afa
kristian4res <57638182+kristian4res@users.noreply.github.com>
2024-06-06 13:47:52 +0100

    feat: add vscode debugger config, test: add missing env vars

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
     moduleNameMapper: {
         "\\.(css|less|scss)$": "identity-obj-proxy",
-        "^axios$": require.resolve("axios")
+        "^axios$": require.resolve("axios"),
     },
-    coveragePathIgnorePatterns: [
-        "/node_modules/"
-    ],
-    testTimeout: 30000
+    coveragePathIgnorePatterns: ["/node_modules/"],
+    testTimeout: 30000,
 };
 
 process.env = Object.assign(process.env, {
@@ -15,5 +13,8 @@ process.env = Object.assign(process.env, {
     BUCKET_NAME: "unique-bucket",
     SERVER_PARK: "server-park",
     BIMS_API_URL: "bims-mock-api",
-    BIMS_CLIENT_ID: "mock-client-id"
+    BIMS_CLIENT_ID: "mock-client-id",
+    SESSION_TIMEOUT: "your-session-timeout",
+    CREATE_DONOR_CASES_CLOUD_FUNCTION_URL: "https://example.cloudfunction.net/",
+    GOOGLE_CLOUD_PROJECT: "example-project-id",
 });
