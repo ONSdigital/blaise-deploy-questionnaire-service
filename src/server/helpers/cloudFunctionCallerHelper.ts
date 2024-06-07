de4399859306aebe26043a8a447b82582662b155
SidraJaved <sidrajaved92@gmail.com>
2024-06-07 10:31:06 +0100

    fix: missed await for getting token call to complete before calling cloud function
    REfs: bLAIS5-4126

--- src/server/helpers/cloudFunctionCallerHelper.ts
+++ src/server/helpers/cloudFunctionCallerHelper.ts
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { GoogleAuth } from "google-auth-library";
 
+async function getIdTokenFromMetadataServer(targetAudience: string) {
+    const googleAuth = new GoogleAuth();
+
+    const client = await googleAuth.getIdTokenClient(targetAudience);
+
+    // Get the ID token.
+    // Once you've obtained the ID token, you can use it to make an authenticated call
+    // to the target audience.
+    const token = await client.idTokenProvider.fetchIdToken(targetAudience);
+    return token;
+}
+
 export async function callCloudFunctionToCreateDonorCases(url: string, payload: any): Promise<string> {
 
-    async function getIdTokenFromMetadataServer(targetAudience: string) {
-        const googleAuth = new GoogleAuth();
-
-        const client = await googleAuth.getIdTokenClient(targetAudience);
-
-        // Get the ID token.
-        // Once you've obtained the ID token, you can use it to make an authenticated call
-        // to the target audience.
-        const token = await client.idTokenProvider.fetchIdToken(targetAudience);
-        return token;
-    }
-    const token = getIdTokenFromMetadataServer(url);
+    const token = await getIdTokenFromMetadataServer(url);
     try {
         const response = await axios.post(url, payload, {
             headers: {
